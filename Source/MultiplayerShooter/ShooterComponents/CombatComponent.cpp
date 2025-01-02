af1de83abec0f66d98c05bca7b03cab4e1e7e3da
KThai01 <kevinathai01@gmail.com>
2025-01-01 23:23:43 -0800

    Reload Animations 3

--- Source/MultiplayerShooter/ShooterComponents/CombatComponent.cpp
+++ Source/MultiplayerShooter/ShooterComponents/CombatComponent.cpp
@@ -159,7 +159,17 @@ void UCombatComponent::EquipWeapon(AWeapon* WeaponToEquip)
 
 void UCombatComponent::Reload()
 {
+	if (CarriedAmmo > 0)
+	{
+		ServerReload();
+	}
+}
 
+void UCombatComponent::ServerReload_Implementation()
+{
+	if (Character == nullptr) return;
+
+	Character->PlayReloadMontage();
 }
 
 void UCombatComponent::OnRep_EquippedWeapon()
--- Source/MultiplayerShooter/ShooterComponents/CombatComponent.h
+++ Source/MultiplayerShooter/ShooterComponents/CombatComponent.h
@@ -52,6 +52,9 @@ protected:
 
 	void SetHUDCrosshairs(float DeltaTime);
 
+	UFUNCTION(Server, Reliable)
+	void ServerReload();
+
 private:
 	UPROPERTY()
 	AShooterCharacter* Character;
